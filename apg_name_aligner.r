130b4bbfb45099c5ef01024be303be6ea4180f28
Oscar <owkien@gmail.com>
2023-10-23 17:01:03 +0200

    Run apg_name_aligner

--- apg_name_aligner.r
+++ apg_name_aligner.r
@@ -38,7 +38,6 @@ wcp[which(wcp$family == "Ripogonaceae"),]$family <- "Rhipogonaceae"
 
 # Tiganophytaceae - includes monotypic genus, but should be alright
 
-
 # Fixing and removal of names / families
 wcp <- wcp[!wcp$family=="Pseudotubulare",]
 
@@ -46,6 +45,22 @@ unique(wcp$family[!wcp$family %in% apg$Syn_Fam])
 
 # This should update family column of our WCVP data to be right according to APGIV
 wcp$family.apg <- apg$Acc_Fam[match(wcp$family, apg$Syn_Fam)]
+
+#Why is there some NA's in the family.apg column?
+# There should'nt be any NA's in the family.apg column.
+no_na_family.apg <- sum(!is.na(wcp$family.apg))
+if ( no_na_family.apg == nrow(wcp)) {
+  cat("There are no NA's in the family.apg column \n")
+} else {
+  cat("There are ", nrow(wcp) - no_na_family.apg, " NA's in the family.apg column \n")
+}
+
+# Printing the rows where the family column is not NA but the family.apg column is NA.
+row_fam_no_fam.apg <- wcp[!is.na(wcp$family) & is.na(wcp$family.apg),]
+cat("Total rows where the family column is not NA but the family.apg column is NA ", nrow(row_fam_no_fam.apg), "\n")
+cat("The rows where the family column is not NA but the family.apg column is NA \n")
+print(row_fam_no_fam.apg)
+
 
 saveRDS(wcp, output_file)
 
