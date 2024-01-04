0f59656374d40839163298e7801f1a6d9daaee18
Oscar <owkien@gmail.com>
2024-01-04 14:19:20 +0100

    Now Clads should have an array of sampling.

--- ClaDs.jl
+++ ClaDs.jl
@@ -2,6 +2,8 @@
 using PANDA
 using JLD2
 using DelimitedFiles
+using Base
+using DataFrames
 
 # Measure the time to load Pkg
 #time_load_pkg = @elapsed using Pkg
@@ -17,12 +19,13 @@ println("Time to load PANDA: $time_load_panda seconds")
 println("Time to load JLD2: $time_load_jld2 seconds")
 
 # Prepare some paths test script locally
- path_to_tree = "/home/au543206/GenomeDK/Trf_models/workflow/02_adding_orders/pruning/orders/pruned_tree_order_Zingiberales_GBMB.tre"
+#path_to_tree = "/home/au543206/GenomeDK/Trf_models/workflow/02_adding_orders/pruning/orders/pruned_tree_order_Zingiberales_GBMB.tre"
+#sampling_freq_file = "/home/au543206/GenomeDK/Trf_models/workflow/03_distribution_data/Zingiberales_sampling_fraction.txt"
 # output_name = "test_output.jld2"
 
 # Fetching arguments
 path_to_tree = ARGS[1]
-sampling_freq = ARGS[2]
+sampling_freq_file = ARGS[2]
 output_name = ARGS[3]
 
 # Measure the time to load the tree
@@ -30,11 +33,31 @@ time_load_tree = @elapsed tree = load_tree(path_to_tree)
 println("Time to load the tree: $time_load_tree seconds")
 
 # Load the array of floats from the sampling_freq file
-sampling_freq = readdlm(sampling_freq)
-sampling_freq = float.(sampling_freq)
+sampling_freq = readdlm(sampling_freq_file)
+name_list_tips = tip_labels(tree)
+
+# Replacing "_" with " " in each entry in name_list_tips
+name_list_tips = replace.(name_list_tips, "_" => " ")
+
+# Add a column to name_list_tips with the numbers 1 to 700.
+name_list_tips = hcat(name_list_tips, 1:700)
+
+# Converting to DataFrames
+sampling_freq = sampling_freq[2:end,:] # Remove the first row with column names
+sampling_freq = DataFrame(sampling_freq, [:species, :frequency])
+name_list_tips = DataFrame(name_list_tips, [:species, :nr_in_tree])
+
+sampling_freq
+name_list_tips
+
+# Join the sampling_freq array with the name_list_tips array by matching the first column in each array.
+sampling_freq_joined = innerjoin(sampling_freq, name_list_tips, on = "species")
+
+# pull out the frequency column as an array
+sampling_freq_array = sampling_freq_joined[!, :frequency]
 
 # Measure the time to run infer_ClaDS
-time_infer = @elapsed output = infer_ClaDS(tree, print_state = 100, f = sampling_freq)
+time_infer = @elapsed output = infer_ClaDS(tree, print_state = 100, f = sampling_freq_array)
 println("Time to run infer_ClaDS: $time_infer seconds")
 
 # Measure the time to save the output
