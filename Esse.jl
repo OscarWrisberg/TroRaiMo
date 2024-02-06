b90d80ecb45c6ac0b1cdbd53edcf21d4316abeba
Oscar <owkien@gmail.com>
2024-02-06 17:37:14 +0100

    Test of Esse with no parallel.

--- Esse.jl
+++ Esse.jl
@@ -32,9 +32,9 @@ hidden_states = parse(Int, ARGS[7])
 
 # The first argument is the number of processors available to the Tapestree
 # Load Tapestree and Distributed
-using Distributed
-addprocs(processors)
-@everywhere using Tapestree
+# using Distributed
+# addprocs(processors)
+# @everywhere using Tapestree
 
 # Load a tree file
 tree = joinpath(tree_file)
@@ -54,19 +54,22 @@ out = joinpath(out_file)
 Tapestree.esse(tree, out, hidden_states,
  	envdata_file = paleo_data, 
 	states_file = states,
-	out_states = out_states,
-	cov_mod = ("s",),
-	parallel = true,
-	mc = "mh",
-	ncch= 3
+	out_states = out_states
 	)
 
+	#
+	# cov_mod = ("s",),
+	# parallel = true,
+	# mc = "mh",
+	# ncch= 3
 
 
 # # Testing Esse using the Tapestree guide
 
 # tree_file = joinpath("/home/au543206/Tapestree.jl/data/tree_50.tre")
-# states_file = joinpath("/home/au543206/Tapestree.jl/data/st2_data.txt")
+# states_file_ignacio = joinpath("/home/au543206/Tapestree.jl/data/st2_data.txt")
+# states_ignacio = readdlm(states_file)
+# states_oscar = readdlm(tip_states_file)
 # envdata_file = joinpath("/home/au543206/Tapestree.jl/data/env_data_2.txt")
 # out_file = *(homedir(),"...")
 # out_states = *(homedir(),"...")
@@ -78,6 +81,7 @@ Tapestree.esse(tree, out, hidden_states,
 
 # Savint the output in R
 r_output = replace(output_name, r"\.jld2" => ".R")
+
 
 
 
