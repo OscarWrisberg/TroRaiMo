7b478c33eff462509bc1f283c71d2388073260d6
Oscar <owkien@gmail.com>
2024-02-06 15:00:42 +0100

    Commented out some testing code.

--- Esse.jl
+++ Esse.jl
@@ -13,13 +13,13 @@ using DelimitedFiles
 using PANDA
 
 # Srun file locations
-processors = 2
-tree_file = "/home/au543206/GenomeDK/Trf_models/workflow/02_adding_orders/pruning/orders/pruned_tree_order_Arecales_GBMB.tre"
-tip_states_file = "/home/au543206/GenomeDK/Trf_models/workflow/03_distribution_data/Arecales_states_0.2.txt"
-paleo_clim_file = "/home/au543206/GenomeDK/Trf_models/TroRaiMo/paleoclim_area.txt"
-out_states_file = "Esse_states_Arecales_0.4.txt"
-out_file = "whatever2"
-hidden_states = 2
+# processors = 2
+# tree_file = "/home/au543206/GenomeDK/Trf_models/workflow/02_adding_orders/pruning/orders/pruned_tree_order_Arecales_GBMB.tre"
+# tip_states_file = "/home/au543206/GenomeDK/Trf_models/workflow/03_distribution_data/Arecales_states_0.2.txt"
+# paleo_clim_file = "/home/au543206/GenomeDK/Trf_models/TroRaiMo/paleoclim_area.txt"
+# out_states_file = "Esse_states_Arecales_0.4.txt"
+# out_file = "whatever2"
+# hidden_states = 2
 
 # Take 6 command line arguments
 processors = parse(Int, ARGS[1])
@@ -62,22 +62,22 @@ Tapestree.esse(tree, out, hidden_states,
 	states_file = states,
 	out_states = out_states,
 	cov_mod = ("s",),
-	#parallel = true,
+	parallel = true,
 	mc = "mh",
-	#ncch= 3
+	ncch= 3
 	)
 
 
 
-# Testing Esse using the Tapestree guide
+# # Testing Esse using the Tapestree guide
 
-tree_file = joinpath("/home/au543206/Tapestree.jl/data/tree_50.tre")
-states_file = joinpath("/home/au543206/Tapestree.jl/data/st2_data.txt")
-envdata_file = joinpath("/home/au543206/Tapestree.jl/data/env_data_2.txt")
-out_file = *(homedir(),"...")
-out_states = *(homedir(),"...")
-esse(tree_file, out_file, 2, envdata_file = envdata_file, 
-  states_file = states_file, out_states = out_states, cov_mod = ("s",))
+# tree_file = joinpath("/home/au543206/Tapestree.jl/data/tree_50.tre")
+# states_file = joinpath("/home/au543206/Tapestree.jl/data/st2_data.txt")
+# envdata_file = joinpath("/home/au543206/Tapestree.jl/data/env_data_2.txt")
+# out_file = *(homedir(),"...")
+# out_states = *(homedir(),"...")
+# esse(tree_file, out_file, 2, envdata_file = envdata_file, 
+#   states_file = states_file, out_states = out_states, cov_mod = ("s",))
 
 # Saving the output in a JLD2 file.
 @save output_name output
